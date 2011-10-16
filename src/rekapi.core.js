8fefe3c2d0ba580aba38795de51797c90e6d1ca4
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-16 16:38:36 -0700

    Added a ctor.

--- src/rekapi.core.js
+++ src/rekapi.core.js
@@ -10,8 +10,23 @@
   
   var gr;
   
-  gr = global.Rekapi || function Rekapi () {
+  /**
+   * @param {HTMLCanvas} canvas
+   * @param {Object} params
+   * @param {Object} events
+   */
+  gr = global.Rekapi || function Rekapi (canvas, params, events) {
+    this.canvas = canvas;
+    this.canvas_context = canvas.getContext('2d');
     
+    return this;
+  };
+  
+  /**
+   * Gets the 2d context of the Rekapi's canvas.
+   */
+  gr.prototype.context = function () {
+    return this.canvas.getContext('2d');
   };
   
   global.Rekapi = gr;
