14f590ff33a341d0ab289904a373d3b00570a214
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-23 19:04:09 -0700

    Refactor.

--- src/rekapi.core.js
+++ src/rekapi.core.js
@@ -12,21 +12,6 @@
       ,defaultConfig
       ,now;
   
-  /**
-   * @param {HTMLCanvas} canvas
-   * @param {string} dimension The dimension (either "height" or "width") to
-   *    get or set.
-   * @param {number} opt_new_size The new value to set for `dimension`.
-   */
-  function canvas_dimension (canvas, dimension, opt_new_size) {
-    if (typeof opt_new_size !== 'undefined') {
-      canvas[dimension] = opt_new_size;
-      canvas.style[dimension] = opt_new_size + 'px';
-    }
-    
-    return canvas[dimension];
-  }
-  
   
   /**
    * Sorts an array numerically, from smallest to largest.
@@ -105,41 +90,9 @@
     return this;
   };
   
-  gk.prototype.height = function (opt_height) {
-    return canvas_dimension(this.canvas, 'height', opt_height);
-  };
-  
-  gk.prototype.width = function (opt_width) {
-    return canvas_dimension(this.canvas, 'width', opt_width);
-  };
   
   gk.prototype.animationLength = function () {
     return this._animationLength;
-  };
-  
-  /**
-   * Get or set a style on the Kapi canvas.
-   * @param {string} styleName
-   * @param {number|string} opt_styleValue The value to set for `styleName`
-   */
-  gk.prototype.canvas_style = function (styleName, opt_styleValue) {
-    if (typeof opt_styleValue !== 'undefined') {
-      this.canvas.style[styleName] = opt_styleValue;
-    }
-    
-    return this.canvas.style[styleName];
-  }
-  
-  
-  gk.prototype.canvas_clear = function () {
-    this.context().clearRect(0, 0, this.width(), this.height());
-  };
-  
-  /**
-   * Gets the 2d context of the Kapi's canvas.
-   */
-  gk.prototype.context = function () {
-    return this.canvas_context;
   };
   
   
