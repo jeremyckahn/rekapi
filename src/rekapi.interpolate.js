90f7dfa705cb1b182ca5eccf41fd83fe18211fe2
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-24 23:04:26 -0700

    Fixed a bug with tweening colors.

--- src/rekapi.interpolate.js
+++ src/rekapi.interpolate.js
@@ -8,20 +8,45 @@
 	
 	function getInterpolatedValues (from, current, to, position, easing) {
 	  
-	  var interpolatedValues;
+	  var R_COLOR_COMPONENT = /^(__r__|__g__|__b__)/
+	      ,interpolatedValues;
 	  
 	  interpolatedValues = {};
 	  
 	  _.each(from, function (val, name) {
-	    interpolatedValues[name] = global.Tweenable.util.tweenProps(position, {
-  			'originalState': from
-  			,'to': to
-  			,'timestamp': 0
-  			,'duration': 1
-  			,'easingFunc': Tweenable.prototype.formula[easing[name]]
-  		}, {
-  			'current': current
-  		})[name];
+	    
+	    // This branching looks weird, but it's done because of the way Shifty
+	    // handles color, and how that interferes with Kapi's handling of 
+	    // multiple easings per tween.  Essentially, Shifty creates properties to 
+	    // tween that do not have corresponding easing strings.
+	    if (name.match(R_COLOR_COMPONENT)) {
+	      
+	      interpolatedValues[name] = global.Tweenable.util.tweenProps(position, {
+    			'originalState': from
+    			,'to': to
+    			,'timestamp': 0
+    			,'duration': 1
+    			// The call to `.slice` removes the __color__ prefix that was
+    			// put there by Shifty.  This causes non-modified color property's
+    			// easing to be used. 
+    			,'easingFunc': Tweenable.prototype.formula[easing[name.slice(5)]]
+    		}, {
+    			'current': current
+    		})[name];
+	      
+	    } else {
+	      interpolatedValues[name] = global.Tweenable.util.tweenProps(position, {
+    			'originalState': from
+    			,'to': to
+    			,'timestamp': 0
+    			,'duration': 1
+    			,'easingFunc': Tweenable.prototype.formula[easing[name]]
+    		}, {
+    			'current': current
+    		})[name];
+	    }
+	    
+	    
 	  });
 	  
 		return interpolatedValues;
@@ -35,10 +60,14 @@
 		current = global.Tweenable.util.simpleCopy({}, from);
 		
 		// Call any data type filters
-		global.Tweenable.util.applyFilter('tweenCreated', current, [current, from, to]);
-		global.Tweenable.util.applyFilter('beforeTween', current, [current, from, to]);
-		interpolatedValues = getInterpolatedValues(from, current, to, position, easing);
-		global.Tweenable.util.applyFilter('afterTween', interpolatedValues, [interpolatedValues, from, to]);
+		global.Tweenable.util.applyFilter('tweenCreated', current, 
+		    [current, from, to]);
+		global.Tweenable.util.applyFilter('beforeTween', current, 
+		    [current, from, to]);
+		interpolatedValues = getInterpolatedValues(
+		    from, current, to, position, easing);
+		global.Tweenable.util.applyFilter('afterTween', interpolatedValues, 
+		    [interpolatedValues, from, to]);
 		
 		return interpolatedValues;
 	}
