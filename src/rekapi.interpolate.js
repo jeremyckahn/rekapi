25a752048f32f30dc59d359f1fac1789b0e040ad
Jeremy Kahn <jeremyckahn@gmail.com>
2011-11-25 18:16:25 -0600

    Updated to use Shifty 0.5.0.

--- src/rekapi.interpolate.js
+++ src/rekapi.interpolate.js
@@ -5,68 +5,7 @@
   var gk;
   
   gk = global.Kapi;
-  
-  function getInterpolatedValues (from, to, position, easing) {
-    
-    var R_COLOR_COMPONENT = /^(__r__|__g__|__b__)/
-        ,interpolatedValues;
-    
-    interpolatedValues = {};
-    
-    _.each(from, function (val, name) {
-      
-      var easingFunc;
-      
-      if (name.match(R_COLOR_COMPONENT)) {
-        // The call to `.slice` removes the __color__ prefix that was
-        // put there by Shifty.  This causes non-modified color properties
-        // easing to be used.
-        easingFunc = Tweenable.prototype.formula[easing[name.slice(5)]];
-      } else {
-        easingFunc = Tweenable.prototype.formula[easing[name]];
-      }
-      
-      if (typeof to[name] !== 'undefined') {
-        interpolatedValues[name] = global.Tweenable.util.tweenProp(
-            from[name]
-            ,to[name]
-            ,easingFunc
-            ,position);
-      }
 
-    });
-    
-    return interpolatedValues;
-  }
-
-  
-  /**
-   * @param {Object} from
-   * @param {Object} to
-   * @param {Object} position
-   * @param {Object} easing
-   * @returns {Object}
-   */
-  function interpolate (from, to, position, easing) {
-    var current,
-      interpolatedValues;
-    
-    current = global.Tweenable.util.simpleCopy({}, from);
-    
-    // Call any data type filters
-    global.Tweenable.util.applyFilter('tweenCreated', current, 
-        [current, from, to]);
-    global.Tweenable.util.applyFilter('beforeTween', current, 
-        [current, from, to]);
-    interpolatedValues = getInterpolatedValues(
-        from, to, position, easing);
-    global.Tweenable.util.applyFilter('afterTween', interpolatedValues, 
-        [interpolatedValues, from, to]);
-    
-    return interpolatedValues;
-  }
-  
-  
   /**
    * @param {Object} to
    * @param {Object} position
@@ -76,7 +15,8 @@
   gk.Actor.prototype.interpolate = function (to, position, easing) {
     var interpolatedValues;
     
-    interpolatedValues = interpolate(this.get(), to, position, easing);
+    interpolatedValues = global.Tweenable.prototype.interpolate.apply(
+        this, arguments);
     this.set(interpolatedValues);
     
     return interpolatedValues;
