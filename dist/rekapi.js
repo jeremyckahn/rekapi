3d494e65815c5867b869c302c74946d0177d9316
Jeremy Kahn <jeremyckahn@gmail.com>
2012-04-01 14:43:28 -0700

    Built 0.5.0.  New minor version because of a change in the directory structure; nonessential modules now live in ext/.

--- dist/rekapi.js
+++ dist/rekapi.js
@@ -1,5 +1,5 @@
 /**
- * Rekapi - Rewritten Kapi. v0.4.16
+ * Rekapi - Rewritten Kapi. v0.5.0
  *   By Jeremy Kahn - jeremyckahn@gmail.com
  *   https://github.com/jeremyckahn/rekapi
  *
@@ -1281,92 +1281,6 @@
    */
 
 } (this));
-;(function rekapiDOM (global) {
-  var gk
-      ,getStyle
-      ,transforms;
-
-  gk = global.Kapi;
-  transforms = [
-    'transform'
-    ,'webkitTransform'
-    ,'MozTransform'
-    ,'oTransform'
-    ,'msTransform'];
-
-  if (!global.getComputedStyle) {
-    return;
-  }
-
-  function getStyle (forElement, styleName) {
-    return global.getComputedStyle(forElement).getPropertyValue(styleName);
-  }
-
-  function setStyle (forElement, styleName, styleValue) {
-    forElement.style[styleName] = styleValue;
-  }
-
-  function hideElement (element) {
-    setStyle(element, 'display', 'none');
-  }
-
-  function showElement (element) {
-    setStyle(element, 'display', 'block');
-  }
-
-  /**
-   * @param {HTMLElement} element
-   * @return {Kapi.DOMActor}
-   */
-  gk.DOMActor = function (element) {
-    var actor;
-
-    actor = new gk.Actor ({
-      'setup': function () {
-        if (getStyle(this.kapi.canvas_getContext(), 'position') === 'static') {
-          setStyle(this.kapi.canvas_getContext(), 'position', 'relative');
-        }
-
-        if (getStyle(element, 'position') === 'static') {
-          setStyle(element, 'position', 'absolute');
-        }
-      }
-
-      ,'draw': function (canvas_context, state) {
-        var isShowing;
-
-        isShowing = false;
-
-        _.each(state, function (styleValue, styleName) {
-          isShowing = true;
-
-          if (styleName === 'rotate') {
-            _.each(transforms, function (transform) {
-              setStyle(element, transform, 'rotate(' + styleValue + 'deg)')
-            });
-          } else {
-            setStyle(element, styleName, styleValue);
-          }
-        });
-
-        isShowing ? showElement(element) : hideElement(element);
-      }
-    });
-
-    actor.show = function (alsoPersist) {
-      gk.Actor.prototype.show.call(this, alsoPersist);
-      showElement(element);
-    };
-
-    actor.hide = function (alsoUnpersist) {
-      gk.Actor.prototype.hide.call(this, alsoUnpersist);
-      hideElement(element);
-    };
-
-    return actor;
-  };
-
-}(this));
 ;(function rekapiCanvas (global) {
 
   var gk,
@@ -1585,6 +1499,92 @@
   };
 
 } (this));
+;(function rekapiDOM (global) {
+  var gk
+      ,getStyle
+      ,transforms;
+
+  gk = global.Kapi;
+  transforms = [
+    'transform'
+    ,'webkitTransform'
+    ,'MozTransform'
+    ,'oTransform'
+    ,'msTransform'];
+
+  if (!global.getComputedStyle) {
+    return;
+  }
+
+  function getStyle (forElement, styleName) {
+    return global.getComputedStyle(forElement).getPropertyValue(styleName);
+  }
+
+  function setStyle (forElement, styleName, styleValue) {
+    forElement.style[styleName] = styleValue;
+  }
+
+  function hideElement (element) {
+    setStyle(element, 'display', 'none');
+  }
+
+  function showElement (element) {
+    setStyle(element, 'display', 'block');
+  }
+
+  /**
+   * @param {HTMLElement} element
+   * @return {Kapi.DOMActor}
+   */
+  gk.DOMActor = function (element) {
+    var actor;
+
+    actor = new gk.Actor ({
+      'setup': function () {
+        if (getStyle(this.kapi.canvas_getContext(), 'position') === 'static') {
+          setStyle(this.kapi.canvas_getContext(), 'position', 'relative');
+        }
+
+        if (getStyle(element, 'position') === 'static') {
+          setStyle(element, 'position', 'absolute');
+        }
+      }
+
+      ,'draw': function (canvas_context, state) {
+        var isShowing;
+
+        isShowing = false;
+
+        _.each(state, function (styleValue, styleName) {
+          isShowing = true;
+
+          if (styleName === 'rotate') {
+            _.each(transforms, function (transform) {
+              setStyle(element, transform, 'rotate(' + styleValue + 'deg)')
+            });
+          } else {
+            setStyle(element, styleName, styleValue);
+          }
+        });
+
+        isShowing ? showElement(element) : hideElement(element);
+      }
+    });
+
+    actor.show = function (alsoPersist) {
+      gk.Actor.prototype.show.call(this, alsoPersist);
+      showElement(element);
+    };
+
+    actor.hide = function (alsoUnpersist) {
+      gk.Actor.prototype.hide.call(this, alsoUnpersist);
+      hideElement(element);
+    };
+
+    return actor;
+  };
+
+}(this));
 ;(function (Rekapi, global) {
 
   // CONSTANTS
