512b66bed53f087a0c3825394992a24d126781fa
sork <franck.git@gmail.com>
2012-06-16 02:19:33 +0200

    Add independent transform properties support to DOMActor.keyframe

--- ext/dom/rekapi.dom.actor.js
+++ ext/dom/rekapi.dom.actor.js
@@ -9,10 +9,62 @@ var rekapiDOM = function (context, _) {
     ,'MozTransform'
     ,'oTransform'
     ,'msTransform'];
+  var transformFunctions = [
+    'translateX',
+    'translateY',
+    'scale',
+    'scaleX',
+    'scaleY',
+    'rotate',
+    'skewX',
+    'skewY'];
 
 
   function setStyle (forElement, styleName, styleValue) {
     forElement.style[styleName] = styleValue;
+  }
+
+
+  /**
+   * @param {string} name A transform function name
+   * @return {boolean}
+   */
+  function isTransformFunction(name) {
+    return _.include(transformFunctions, name);
+  }
+
+
+  /**
+   * Builds a concatenated string of given transform property values in order.
+   *
+   * @param {Array} orderedFunctions Array of ordered transform function names
+   * @param {Object} transformProperties Transform properties to build together
+   * @return {string}
+   */
+  function buildTransformValue(orderedFunctions, transformProperties) {
+    var transformComponents = [];
+
+    _.each(orderedFunctions, function(functionName) {
+      if (transformProperties[functionName]) {
+        transformComponents.push(functionName + '(' +
+          transformProperties[functionName] + ')');
+      }
+    });
+
+    return transformComponents.join(' ');
+  }
+
+
+  /**
+   * Sets value for all vendor prefixed transform properties on a given context
+   *
+   * @param {Object} context The actor's DOM context
+   * @param {string} transformValue The transform style value
+   */
+  function setTransformStyles(context, transformValue) {
+    _.each(transforms, function(prefixedTransform) {
+      setStyle(context, prefixedTransform, transformValue);
+    });
   }
 
 
@@ -30,6 +82,8 @@ var rekapiDOM = function (context, _) {
     if (!this._context.className.match(className)) {
       this._context.className += ' ' + className;
     }
+
+    this._transformOrder = transformFunctions;
 
     // Remove the instance's update method to allow the
     // ActorMethods.prototype.update method to be accessible.
@@ -50,15 +104,20 @@ var rekapiDOM = function (context, _) {
    * @param {Object} state
    */
   DOMActorMethods.prototype.update = function (context, state) {
+    var propertyNames = _.keys(state);
+    var transformFunctionNames = _.filter(propertyNames, isTransformFunction);
+    var otherPropertyNames = _.reject(propertyNames, isTransformFunction);
+    var otherProperties = _.pick(state, otherPropertyNames);
 
-    _.each(state, function (styleValue, styleName) {
-      if (styleName === 'transform') {
-        _.each(transforms, function (transform) {
-          setStyle(context, transform, styleValue);
-        }, this);
-      } else {
-        setStyle(context, styleName, styleValue);
-      }
+    if (transformFunctionNames.length > 0) {
+      setTransformStyles(context, buildTransformValue(this._transformOrder,
+        _.pick(state, transformFunctionNames)));
+    } else if (state.transform) {
+      setTransformStyles(context, state.transform);
+    }
+
+    _.each(otherProperties, function (styleValue, styleName) {
+      setStyle(context, styleName, styleValue);
     }, this);
   };
 
@@ -75,6 +134,26 @@ var rekapiDOM = function (context, _) {
    */
   DOMActorMethods.prototype.getCSSName = function () {
     return 'actor-' + this.id;
+  };
+
+
+  /**
+   * Overrides the default transform function order.
+   * 
+   * @param {Array} orderedFunctions The Array of transform function names
+   * @return {Kapi}
+   */
+  DOMActorMethods.prototype.setTransformOrder = function (orderedFunctions) {
+    var unknownFunctions = _.reject(orderedFunctions, isTransformFunction);
+
+    if (unknownFunctions.length > 0) {
+      throw new Error('Unknown or unsupported transform functions: ' +
+        unknownFunctions.join(', '));
+    }
+    // Ignore duplicate transform function names in the array
+    this._transformOrder = _.uniq(orderedFunctions);
+
+    return this;
   };
 
 };
