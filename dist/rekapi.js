c39ceafbae3ba26e9c9efefd4d845e62cd717db6
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-21 19:26:15 -0700

    Builds v.0.9.9.

--- dist/rekapi.js
+++ dist/rekapi.js
@@ -1,6 +1,6 @@
 /*jslint browser: true, nomen: true, plusplus: true, undef: true, vars: true, white: true */
 /**
- * Rekapi - Rewritten Kapi. v0.9.8 (Thu, 21 Jun 2012 03:38:37 GMT)
+ * Rekapi - Rewritten Kapi. v0.9.9 (Fri, 22 Jun 2012 02:21:16 GMT)
  * https://github.com/jeremyckahn/rekapi
  *
  * By Jeremy Kahn (jeremyckahn@gmail.com), with significant contributions from
@@ -1546,16 +1546,69 @@ var rekapiDOM = function (context, _) {
   'use strict';
 
   var Kapi = context.Kapi;
+  // TODO:  Change the name of this array to a clearer name, e.g. `vendorTransforms`
   var transforms = [
     'transform'
     ,'webkitTransform'
     ,'MozTransform'
     ,'oTransform'
     ,'msTransform'];
+  var transformFunctions = [
+    'translateX',
+    'translateY',
+    'scale',
+    'scaleX',
+    'scaleY',
+    'rotate',
+    'skewX',
+    'skewY'];
 
 
   function setStyle (forElement, styleName, styleValue) {
     forElement.style[styleName] = styleValue;
+  }
+
+
+  /**
+   * @param {string} name A transform function name
+   * @return {boolean}
+   */
+  function isTransformFunction (name) {
+    return _.contains(transformFunctions, name);
+  }
+
+
+  /**
+   * Builds a concatenated string of given transform property values in order.
+   *
+   * @param {Array.<string>} orderedFunctions Array of ordered transform function names
+   * @param {Object} transformProperties Transform properties to build together
+   * @return {string}
+   */
+  function buildTransformValue (orderedFunctions, transformProperties) {
+    var transformComponents = [];
+
+    _.each(orderedFunctions, function(functionName) {
+      if (transformProperties[functionName]) {
+        transformComponents.push(functionName + '(' +
+          transformProperties[functionName] + ')');
+      }
+    });
+
+    return transformComponents.join(' ');
+  }
+
+
+  /**
+   * Sets value for all vendor prefixed transform properties on a given context
+   *
+   * @param {Object} context The actor's DOM context
+   * @param {string} transformValue The transform style value
+   */
+  function setTransformStyles (context, transformValue) {
+    _.each(transforms, function(prefixedTransform) {
+      setStyle(context, prefixedTransform, transformValue);
+    });
   }
 
 
@@ -1573,6 +1626,8 @@ var rekapiDOM = function (context, _) {
     if (!this._context.className.match(className)) {
       this._context.className += ' ' + className;
     }
+
+    this._transformOrder = transformFunctions.slice(0);
 
     // Remove the instance's update method to allow the
     // ActorMethods.prototype.update method to be accessible.
@@ -1593,15 +1648,22 @@ var rekapiDOM = function (context, _) {
    * @param {Object} state
    */
   DOMActorMethods.prototype.update = function (context, state) {
+    var propertyNames = _.keys(state);
+    // TODO:  Optimize the following code so that propertyNames is not looped over twice.
+    var transformFunctionNames = _.filter(propertyNames, isTransformFunction);
+    var otherPropertyNames = _.reject(propertyNames, isTransformFunction);
+    var otherProperties = _.pick(state, otherPropertyNames);
 
-    _.each(state, function (styleValue, styleName) {
-      if (styleName === 'transform') {
-        _.each(transforms, function (transform) {
-          setStyle(context, transform, styleValue);
-        }, this);
-      } else {
-        setStyle(context, styleName, styleValue);
-      }
+    if (transformFunctionNames.length) {
+      var transformProperties = _.pick(state, transformFunctionNames);
+      var builtStyle = buildTransformValue(this._transformOrder, transformProperties);
+      setTransformStyles(context, builtStyle);
+    } else if (state.transform) {
+      setTransformStyles(context, state.transform);
+    }
+
+    _.each(otherProperties, function (styleValue, styleName) {
+      setStyle(context, styleName, styleValue);
     }, this);
   };
 
@@ -1618,6 +1680,25 @@ var rekapiDOM = function (context, _) {
    */
   DOMActorMethods.prototype.getCSSName = function () {
     return 'actor-' + this.id;
+  };
+
+
+  /**
+   * Overrides the default transform function order.
+   * 
+   * @param {Array} orderedFunctions The Array of transform function names
+   * @return {Kapi}
+   */
+  DOMActorMethods.prototype.setTransformOrder = function (orderedFunctions) {
+    var unknownFunctions = _.reject(orderedFunctions, isTransformFunction);
+
+    if (unknownFunctions.length) {
+      throw 'Unknown or unsupported transform functions: ' + unknownFunctions.join(', ');
+    }
+    // Ignore duplicate transform function names in the array
+    this._transformOrder = _.uniq(orderedFunctions);
+
+    return this;
   };
 
 };
