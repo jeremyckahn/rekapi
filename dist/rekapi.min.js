54597af5a83d69a12beadf8ff8a3901cbc6efd70
Jeremy Kahn <jeremyckahn@gmail.com>
2012-11-10 17:49:43 -0800

    Updates Shifty to 0.7.0.

--- dist/rekapi.min.js
+++ dist/rekapi.min.js
@@ -1,6 +1,6 @@
 /*jslint browser: true, nomen: true, plusplus: true, undef: true, vars: true, white: true */
 /**
- * Rekapi - Rewritten Kapi. v0.12.1 (Sun, 15 Jul 2012 17:20:15 GMT)
+ * Rekapi - Rewritten Kapi. v0.12.2 (Sun, 11 Nov 2012 01:48:10 GMT)
  * https://github.com/jeremyckahn/rekapi
  *
  * By Jeremy Kahn (jeremyckahn@gmail.com), with significant contributions from
