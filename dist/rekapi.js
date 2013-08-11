0f0c97574ba7fafe6df3d614aa48b19e89dea135
Jeremy Kahn <jeremyckahn@gmail.com>
2013-08-10 17:37:18 -0700

    Fixes bug where 'rotate(3deg)' would break.

--- dist/rekapi.js
+++ dist/rekapi.js
@@ -1,4 +1,4 @@
-/*! Rekapi - v0.15.13 - 2013-08-04 - http://rekapi.com */
+/*! Rekapi - v0.15.14 - 2013-08-10 - http://rekapi.com */
 /*!
  * Rekapi - Rewritten Kapi.
  * https://github.com/jeremyckahn/rekapi
@@ -2093,7 +2093,7 @@ var rekapiDOM = function (context, _) {
   /*!
    * transform properties like translate3d and rotate3d break the cardinality
    * of multi-ease easing strings, because the "3" gets treated like a
-   * tweenable value.  Transform "3d" to "__THREED__" to prevent this, and
+   * tweenable value.  Transform "3d(" to "__THREED__" to prevent this, and
    * transform it back in _afterKeyframePropertyInterpolate.
    *
    * @param {Kapi.KeyframeProperty} keyframeProperty
@@ -2108,9 +2108,9 @@ var rekapiDOM = function (context, _) {
     var value = keyframeProperty.value;
     var nextProp = keyframeProperty.nextProperty;
 
-    if (nextProp && value.match(/3d/g)) {
-      keyframeProperty.value = value.replace(/3d/g, '__THREED__');
-      nextProp.value = nextProp.value.replace(/3d/g, '__THREED__');
+    if (nextProp && value.match(/3d\(/g)) {
+      keyframeProperty.value = value.replace(/3d\(/g, '__THREED__');
+      nextProp.value = nextProp.value.replace(/3d\(/g, '__THREED__');
     }
   };
 
@@ -2130,11 +2130,11 @@ var rekapiDOM = function (context, _) {
     var nextProp = keyframeProperty.nextProperty;
 
     if (nextProp && value.match(/__THREED__/g)) {
-      keyframeProperty.value = value.replace(/__THREED__/g, '3d');
-      nextProp.value = nextProp.value.replace(/__THREED__/g, '3d');
+      keyframeProperty.value = value.replace(/__THREED__/g, '3d(');
+      nextProp.value = nextProp.value.replace(/__THREED__/g, '3d(');
       var keyPropName = keyframeProperty.name;
       interpolatedObject[keyPropName] =
-          interpolatedObject[keyPropName].replace(/__THREED__/g, '3d');
+          interpolatedObject[keyPropName].replace(/__THREED__/g, '3d(');
     }
   };
 
