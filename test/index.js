caa9ae82f17d9dafa1832ab377201f0ac67d6f0c
Jeremy Kahn <jeremyckahn@gmail.com>
2017-04-22 10:50:06 -0500

    Port addActor test

--- test/index.js
+++ test/index.js
@@ -3,9 +3,25 @@ import assert from 'assert';
 import Rekapi from '../src/main';
 
 describe('Rekapi', () => {
+  const setupTestRekapi = () => new Rekapi();
+  const setupTestActor = rekapi => rekapi.addActor(new Rekapi.Actor());
+
+  let rekapi, actor;
+
   describe('constructor', () => {
     it('is a function', () => {
       assert.equal(typeof Rekapi, 'function');
+    });
+  });
+
+  describe('#addActor', () => {
+    beforeEach(() => {
+      rekapi = setupTestRekapi();
+      actor = setupTestActor(rekapi);
+    });
+
+    it('adds actors', () => {
+      assert.equal(rekapi._actors[actor.id], actor);
     });
   });
 });
