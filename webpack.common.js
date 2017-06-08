02b7a528b14831c10ebfe19128a992c0115060a7
Jeremy Kahn <jeremyckahn@gmail.com>
2017-06-08 08:31:08 -0500

    Upgrade to Webpack 2

--- webpack.common.js
+++ webpack.common.js
@@ -6,30 +6,17 @@ const modulePaths = [
 
 module.exports = {
   devtool: 'source-map',
-  resolveLoader: {
-    // http://webpack.github.io/docs/troubleshooting.html#npm-linked-modules-doesn-t-find-their-dependencies
-    fallback: modulePaths
-  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          // The commented line below _should_ be all that's necessary, but the
-          // require.resolve below is needed as a workaround for a babel bug:
-          // https://github.com/babel/babel-loader/issues/149#issuecomment-170244381
-          //presets: ['es2015']
-          presets: [require.resolve('babel-preset-es2015')]
-        }
+        use: 'babel-loader'
       }
     ]
   },
   resolve: {
-    modulesDirectories: modulePaths,
-
-    // http://webpack.github.io/docs/troubleshooting.html#npm-linked-modules-doesn-t-find-their-dependencies
-    fallback: modulePaths
-  },
+    modules: [
+      'node_modules'
+    ]
+  }
 };
