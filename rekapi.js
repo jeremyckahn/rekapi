0085cf0ba8f1fbf60dc1cf8ca8acd963d33c96dc
Jeremy Kahn <jeremyckahn@gmail.com>
2017-09-02 21:13:17 -0700

    Updates

--- rekapi.js
+++ rekapi.js
@@ -1,4 +1,4 @@
-/*! 2.0.0-0 */
+/*! 2.0.1-0 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
